e7ad726abe696bfbf88a4ffc1668e33e7d1399ae
이동훈 <ehdgns1766@naver.com>
2019-03-11 06:10:32 +0000

    first commit

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,28 @@
+class MainController < ApplicationController
+  def index
+  end
+  
+  
+  
+  def signup_1
+    
+  end
+  
+  def signup_2
+  end
+  
+  def signup_2_c
+    
+  end
+  
+  def signup_3
+    
+  end
+  
+  def match2
+  end
+  
+  def match3
+  end
+  
+end
